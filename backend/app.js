979074532a0cace413737e5a1209bac2c879642a
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2025-03-22 08:23:49 +0200

    server setup

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+// Настройка на конфигурацията от .env файл
+dotenv.config()
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
