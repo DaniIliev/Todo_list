ad43a93deec48878f2aad9452e55e7bf9a6121c3
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2025-03-22 17:43:49 +0200

    add routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,16 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
+const authRoutes = require('./routes/auth');
+const projectRoutes = require('./routes/projects');
+const taskRoutes = require('./routes/tasks');
+const tagRoutes = require('./routes/tags');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/tags', tagRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
