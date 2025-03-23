f6ce651df264a64ad2f365b91abc0011b1749ad3
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2025-03-23 21:15:54 +0200

    add styles

--- frondend/src/types/index.ts
+++ frondend/src/types/index.ts
@@ -1,4 +1,10 @@
+export interface UserContextType {  
+  user: User | null;  
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;  
+}  
+
+
 export interface User {  
     id: number;  
     username: string;  
-  }  
+}   
